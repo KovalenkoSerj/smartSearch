4621e712fc0c72635be894412be1ca64ce4cd465
Serhii <spekt.92@gmail.com>
2018-05-13 12:57:21 +0300

    show-hide search suggestions

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function findMatches(wordToMatch, cities) {
     return cities.filter(place => {
         var regex = new RegExp(wordToMatch, 'gi');
         return place.city.match(regex) || place.state.match(regex);
+
     });
 }
 
@@ -18,21 +19,21 @@ function showMatches (){
     var matchArray = findMatches(this.value, cities);
     suggestions.innerHTML = matchArray.map(place => {
         var regex = new RegExp (this.value, 'gi');
-        cityName = place.city.replace(regex, `<span class = 'h1'>${this.value}</span>`)
+        cityName = place.city.replace(regex, `<span class = 'h1'>${this.value}</span>`);
         stateName = place.state.replace(regex, `<span class = 'h1'>${this.value}</span>` );
+        if (this.value === '')  suggestions.style.display = 'none';
+        else if (this.value !== '')  suggestions.style.display = 'block';
     return`
-            <li>
+            <li class = 'li'>
             <span class = 'name'>${cityName}, ${stateName}</span>
             </li>
         `;
 
-
     }).join('');
 }
 
-
 var suggestions = document.querySelector('.suggestions');
-document.querySelector('.search').addEventListener('keyup', showMatches);
+var input =document.querySelector('.search').addEventListener('keyup', showMatches);
 
 
 
